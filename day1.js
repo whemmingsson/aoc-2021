257191aa95ff6f98a232e5f3389b52b4686abc81
William Hemmingsson <william.hemmingsson@storytel.com>
2021-12-01 15:06:51 +0100

    Refactoring again for day 1

--- day1.js
+++ day1.js
@@ -2,12 +2,12 @@ const values = require("./util/loader.js").getIntegers("./data/data1");
 const countIncreases = (arr) => arr.filter((e, i, a) => i > 0 && e > a[i - 1]).length;
 
 // Part 1
-console.log(countIncreases(values));
+console.log("Part 1", countIncreases(values));
 
 // Part 2
 let slidingWindow = [];
 for (let i = 0; i < values.length - 2; i++) {
-  slidingWindow.push(values[i] + values[i + 1] + values[i + 2]);
+  slidingWindow.push(values.slice(i, i + 3).reduce((s, v) => s + v, 0));
 }
 
-console.log(countIncreases(slidingWindow));
+console.log("Part 2", countIncreases(slidingWindow));
