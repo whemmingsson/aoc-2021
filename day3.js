18c9e41e986048ec003a66c241f4dbdfd9bf2fd7
William Hemmingsson <william.hemmingsson@storytel.com>
2021-12-03 07:17:11 +0100

    Day 3 - quick and dirty version

---
+++ day3.js
@@ -0,0 +1,51 @@
+const values = require("./util/loader.js").getStrings("./data/data3");
+
+// Binary number
+let gammaRateBinary = ""; // Most common
+
+// Assume all numbers are of same length
+let length = values[0].length;
+
+// Number of values
+let count = values.length;
+
+// Part one
+for (let i = 0; i < length; i++) {
+  let ones = values.map((v) => parseInt(v[i])).filter((v) => v === 1).length;
+  gammaRateBinary += ones > count / 2 ? "1" : "0";
+}
+
+var gammaRate = parseInt(gammaRateBinary, 2);
+var epsilonRate = Math.pow(2, length) - 1 - gammaRate;
+
+console.log(gammaRate * epsilonRate);
+
+//Part two
+let oxygenNumbers = [...values];
+let co2Numbers = [...values];
+for (let i = 0; i < length; i++) {
+  let ones = oxygenNumbers.filter((v) => v[i] === "1");
+  let zeros = oxygenNumbers.filter((v) => v[i] === "0");
+  if (ones.length === oxygenNumbers.length / 2) oxygenNumbers = ones;
+  else oxygenNumbers = ones.length > oxygenNumbers.length / 2 ? ones : zeros;
+
+  if (oxygenNumbers.length === 1) {
+    break;
+  }
+}
+
+for (let i = 0; i < length; i++) {
+  if (co2Numbers.length === 1) {
+    break;
+  }
+
+  let ones = co2Numbers.filter((v) => v[i] === "1");
+  let zeros = co2Numbers.filter((v) => v[i] === "0");
+  if (ones.length === co2Numbers.length / 2) co2Numbers = zeros;
+  else co2Numbers = ones.length < co2Numbers.length / 2 ? ones : zeros;
+}
+
+console.log(oxygenNumbers[0]);
+console.log(co2Numbers[0]);
+
+console.log(parseInt(oxygenNumbers[0], 2) * parseInt(co2Numbers[0], 2));
