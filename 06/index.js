2adc1573c4d682aaabcf25a025064bab1beb4f78
William Hemmingsson <william.hemmingsson@storytel.com>
2021-12-07 07:59:18 +0100

    Day 6

--- 06/index.js
+++ 06/index.js
@@ -1,5 +1,5 @@
 const data = require("../util/loader.js")
-  .getStrings("06/example")[0]
+  .getStrings("06/data")[0]
   .split(",")
   .map((v) => parseInt(v));
 
@@ -33,41 +33,68 @@ console.log("Result part 1: " + fishes.length);
 fishes = [...data];
 let totalFishCount = fishes.length;
 
+// Initialize the "buckets"
 let map = {};
 for (let i = 0; i <= 8; i++) {
   map[i.toString()] = 0;
 }
 
+// Add initial fishes to buckets
 for (let i = 0; i < fishes.length; i++) {
-  let fish = fishes[i];
+  const fish = fishes[i];
   map[fish]++;
-}
-
-function next(k) {
-  return (parseInt(k) + 1).toString();
 }
 
 console.log("Initial value");
 console.log(map);
 
-for (let i = 0; i < 256; i++) {
-  let value = map["0"];
+let fishCount = 0;
+
+for (let i = 0; i < 20; i++) {
+  fishCount = map["0"];
 
   // Move all values down 1 day
   for (let k = 0; k <= 7; k++) {
-    let key = k.toString();
+    const key = k.toString();
     const nextKey = next(key);
     map[key] = map[nextKey];
   }
 
-  if (value > 0) {
-    totalFishCount += value; // Increase total fish count
-    map["6"] += value; // "Reset" a value number of fish
-    map["8"] += value; // Spawn a value number of new fish
-    //map["0"] = 0;
+  if (fishCount > 0) {
+    totalFishCount += fishCount; // Increase total fish count
+    map["6"] += fishCount; // "Reset" a value number of fish
+    map["8"] = fishCount; // Spawn a value number of new fish
   }
 
+  /// ----- DEBUGGING ------ ////
+
   console.log("Day" + (i + 1), totalFishCount);
+  print(map);
+
+  if (i == 79 && totalFishCount == 5934) {
+    console.log("SUCCESS");
+  } else if (i == 79 && totalFishCount != 5934) {
+    console.log("FAIL");
+    break;
+  }
 }
 
 console.log("Result part 2: " + totalFishCount);
+
+function next(k) {
+  return (parseInt(k) + 1).toString();
+}
+
+function print(map) {
+  console.log();
+  console.log([...Array(9).keys()].join(" "));
+
+  let values = "";
+
+  for (const [, value] of Object.entries(map)) {
+    values += value + " ";
+  }
+
+  console.log(values);
+  console.log();
+}
